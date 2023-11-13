ef4a03b3c5d31e1bff609191bd0c5a48fb75a6a8
Magomed Kostoev <m.kostoev@tarantool.org>
2023-11-13 18:56:34 +0300

    Update the plots.

--- functions.sh
+++ functions.sh
@@ -1,9 +1,11 @@
-function gen2 {
-    gnuplot -e "input_file='data/${1}_${2}_${3}.txt'; output_file='plots/${1}_${2}_${3}.png'; xbegin=$4; xend=$5; brief='$1'; main_title='Tarantool $2'" plot.p
-}
-
-function gen3 {
-    gen2 "$@" && firefox "plots/${1}_${2}_${3}.png";
+function gen_single_plot {
+    local x_label="Latency (µs)"
+    local y_label="Percentile"
+    if [[ "$3" == hist ]]; then
+        y_label="Requests"
+    fi
+    gnuplot -e "input_file='data/${1}_${2}_${3}.txt'; output_file='plots/${1}_${2}_${3}.png'; xbegin=$4; xend=$5; brief='$1'; main_title='Tarantool $2'; x_label='$x_label'; y_label='$y_label'" plot.p &&
+    firefox "plots/${1}_${2}_${3}.png" &
 }
 
 function gen4 {
@@ -18,7 +20,7 @@ function gen4 {
     firefox "plots/all_${v}_hist.png" &
 }
 
-function gen5 {
+function gen_vs_plot {
     local func="$1";
     local yend="$2";
     gnuplot -e "input_a='data/${func}_1.5_rhist.txt'; \
@@ -28,32 +30,32 @@ function gen5 {
 }
 
 function gen_vs_plots {
-    gen5 ping 40000;
-    gen5 insert 60000;
-    gen5 select 40000;
-    gen5 delete 60000;
+    gen_vs_plot ping 40000;
+    gen_vs_plot insert 60000;
+    gen_vs_plot select 40000;
+    gen_vs_plot delete 60000;
 }
 
-function gen {
-    gen2 ping 1.5 hist 19500 20500
-    gen2 ping 1.5 cdf 19500 20500
-    gen2 insert 1.5 hist 34750 41000
-    gen2 insert 1.5 cdf 34750 41000
-    gen2 select 1.5 hist 19500 21500
-    gen2 select 1.5 cdf 19500 21500
-    gen2 delete 1.5 hist 34750 40000
-    gen2 delete 1.5 cdf 34750 40000
-    gen2 ping 3.0 hist 32570 33570
-    gen2 ping 3.0 cdf 32570 33570
-    gen2 insert 3.0 hist 49500 54500
-    gen2 insert 3.0 cdf 49500 54500
-    gen2 delete 3.0 hist 49500 54500
-    gen2 delete 3.0 cdf 49500 54500
-    gen2 select 3.0 hist 33100 34200
-    gen2 select 3.0 cdf 33100 34200
-    return
-    i=1.5
-    j=ping
-    i=3.0
-    j=delete
+function gen_single_plots {
+    gen_single_plot ping 1.5 hist 19500 20500
+    gen_single_plot ping 1.5 cdf 19500 20500
+    gen_single_plot insert 1.5 hist 34750 41000
+    gen_single_plot insert 1.5 cdf 34750 41000
+    gen_single_plot select 1.5 hist 19500 21500
+    gen_single_plot select 1.5 cdf 19500 21500
+    gen_single_plot delete 1.5 hist 34750 40000
+    gen_single_plot delete 1.5 cdf 34750 40000
+    gen_single_plot ping 3.0 hist 32570 33570
+    gen_single_plot ping 3.0 cdf 32570 33570
+    gen_single_plot insert 3.0 hist 49500 54500
+    gen_single_plot insert 3.0 cdf 49500 54500
+    gen_single_plot delete 3.0 hist 49500 54500
+    gen_single_plot delete 3.0 cdf 49500 54500
+    gen_single_plot select 3.0 hist 33100 34200
+    gen_single_plot select 3.0 cdf 33100 34200
+}
+
+function gen_all_plots {
+    gen_single_plots;
+    gen_vs_plots;
 }
--- plot.p
+++ plot.p
@@ -4,7 +4,9 @@ set title main_title
 set grid
 set terminal png size 1024,768
 set output sprintf('%s', output_file)
-set xrange [xbegin:xend]
+set xrange [xbegin/1000.0:xend/1000.0]
+set xlabel sprintf('%s', x_label)
+set ylabel sprintf('%s', y_label)
 set key left top
 
-plot input_file pointtype 7 pointsize 1 title brief
+plot input_file using ($1/1000.0):($2) pointtype 7 pointsize 1 title brief
